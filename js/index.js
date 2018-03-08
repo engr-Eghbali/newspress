0aaddf2506f05a21ec82c41ab986e72b4aaa1dba
reza-eghbali <engr.eghbali@gmail.com>
2018-03-08 23:09:09 +0330

    edit btn added

--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
 
-function increaseValue() {
-  var value = parseInt(document.getElementById('number').value, 10);
+function increaseValue(id) {
+  var value = parseInt(document.getElementById(id).value, 10);
   value = isNaN(value) ? 0 : value;
   value++;
-  document.getElementById('number').value = value;
+  document.getElementById(id).value = value;
 }
 
-function decreaseValue() {
-  var value = parseInt(document.getElementById('number').value, 10);
+function decreaseValue(id) {
+  var value = parseInt(document.getElementById(id).value, 10);
   value = isNaN(value) ? 0 : value;
   value < 1 ? value = 1 : '';
   value--;
-  document.getElementById('number').value = value;
+  document.getElementById(id).value = value;
 }
 
 
